6f052ddfd2b45c7d40b4d889c971e168d40e5522
github naotake32 <take32.nao@gmail.com>
2022-12-16 01:45:00 -0800

    created most part of app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
 import user from "../../images/user.png";
 import "./Header.scss";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo">No Movie No Life</div>
-            <div className="user-image">
-                <img src={user} alt="user" />
-            </div>
-        </div>
-    )
-}
+  const [term, setTerm] = useState("");
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (term === "") return alert("Please enter search term!");
+    dispatch(fetchAsyncMovies(term));
+    dispatch(fetchAsyncShows(term));
+    setTerm("");
+  };
+  return (
+    <div className="header">
+      <div className="logo">
+        <Link to="/">Movie App</Link>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={term}
+            placeholder="Search Movies or Shows"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
